cb6a98126c7ceb93af39395ee1b3f0bae533ffa8
Nikos Giakoumoglou <ngiakoumoglou@hotmail.com>
2021-05-29 14:57:52 +0300

    Add files via upload

--- SpatialBasedFeatures/LaterTexture/__init__.py
+++ SpatialBasedFeatures/LaterTexture/__init__.py
@@ -1,7 +1,7 @@
-from .hos import hos_features
+from .hos import hos_features, plot_sinogram
 from .lbp import lbp_features
 from .glszm import glszm_features
 
-__all__ = ['hos_features',
+__all__ = ['hos_features','plot_sinogram',
            'lbp_features',
            'glszm_features']
