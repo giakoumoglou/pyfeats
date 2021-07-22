550bc285fc3ea4badc7d66d29dc7c9f6b3f1fc8d
Nikos Giakoumoglou <ngiakoumoglou@hotmail.com>
2021-07-22 18:55:57 +0300

    Delete SinglePointBasedFeatures directory

--- SinglePointBasedFeatures/HistogramBasedFeatures/__init__.py
+++
@@ -1,7 +0,0 @@
-from .histogram import histogram, plot_histogram
-from .multiregion_histogram import multiregion_histogram
-from .correlogram import correlogram, plot_correlogram
-
-__all__ = ['histogram', 'plot_histogram',
-           'multiregion_histogram',
-           'correlogram', 'plot_correlogram']
--- SinglePointBasedFeatures/SingleMultiROI/README.md
+++
@@ -1,5 +0,0 @@
-### A.1  Single/Multi ROI
-1. GSM
-2. Stratified GSM
-3. JBA
-4. PW
